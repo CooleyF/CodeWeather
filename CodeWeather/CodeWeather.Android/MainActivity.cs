31568f8b94f6b218a060f98acb685622180d3851
CooleyF <60631420+CooleyF@users.noreply.github.com>
2020-07-01 17:33:59 -0400

    Add App center SDK

--- CodeWeather/CodeWeather.Android/MainActivity.cs
+++ CodeWeather/CodeWeather.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace CodeWeather.Droid
 {
@@ -14,6 +17,10 @@ namespace CodeWeather.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            AppCenter.Start("cd68a4b0-a978-4d81-82c7-50644670bbd0",
+                   typeof(Analytics), typeof(Crashes));
+
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
